fd6e8d2676c45f403cd122634cc0d737cdfc1280
Rohan Kulkarni <rohank2502@gmail.com>
2023-04-07 22:15:59 +0530

    limit

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -7,32 +7,7 @@ const CustomError = require("../utils/customError");
 const needle = require("needle");
 
 const userId = "1443953949627854854";
-const url = `https://api.twitter.com/2/users/${userId}/mentions`;
-const url1 = "https://api.twitter.com/2/users/:hacktheleague";
-
-const gestch =async ( )=> {
-  const options = {
-    headers: {
-      "User-Agent": "v2UserMentionssJS",
-      authorization: `Bearer ${process.env.BEARER}`,
-    },
-  };
-  try {
-    const resp = await needle("get", url1, params, options);
-
-    if (resp.statusCode != 200) {
-      console.log(`${resp.statusCode} ${resp.statusMessage}:\n${resp.body}`);
-      // return;
-    }
-    // return resp.body;
-  } catch (err) {
-
-  }
-}
-
-
-
-
+const url = `https://api.twitter.com/2/users/${userId}/mentions?max_results=50`;
 // The code below sets the bearer token from your environment variables
 // To set environment variables on macOS or Linux, run the export command below from the terminal:
 // export BEARER_TOKEN='YOUR-TOKEN'
@@ -40,7 +15,6 @@ const bearerToken = process.env.BEARER;
 
 // this is the ID for @TwitterDev
 const getUserMentions = async () => {
-  gestch();
   let userMentions = [];
   let params = {
     max_results: 100,
